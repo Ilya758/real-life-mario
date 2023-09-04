cd68416463304c96c7de7bdae3f0bcd462cfd941
Illia Skaryna <subscriber6436@gmail.com>
2023-09-04 20:11:42 +0300

    feat: added assets

--- src/main.py
+++ src/main.py
@@ -14,13 +14,13 @@ class Game:
         pygame.display.set_caption('REAL-TIME-MARIO')
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.offsetX = 0
+        self.player = Player(0, 450, 50, 50)
         self.handleGameLoop()
 
     def handleGameLoop(self):
         clock = pygame.time.Clock()
         bg, image = GraphicsService.getBackground('Blue.png')
         run = True
-        player = Player(100, 100, 50, 50)
         fire = Fire(100, HEIGHT - BLOCK_SIZE - 64, 16, 32)
         fire.on()
         floor = [Block(i * BLOCK_SIZE, HEIGHT - BLOCK_SIZE, BLOCK_SIZE)
@@ -32,26 +32,29 @@ class Game:
             clock.tick(FPS)
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    break
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP and player. jump_count < 2:
-                        player.jump()
+                match event.type:
+                    case pygame.QUIT:
+                        run = False
+                        break
+                    case pygame.KEYDOWN:
+                        if event.key == pygame.K_UP and self.player.jump_count < 2:
+                            self.player.jump()
+                    case _:
+                        continue
 
-            player.loop(FPS)
+            self.player.loop(FPS)
             fire.loop()
-            MovementService.handleMove(player, objects)
+            MovementService.handleMove(self.player, objects)
             GraphicsService.draw(self.window, bg, image,
-                                 player, objects, self.offsetX)
-
-            self.handleBackgroundScrolling(player)
+                                 self.player, objects, self.offsetX)
+            self.handleBackgroundScrolling(self.player)
 
         pygame.quit()
         quit()
 
     def handleBackgroundScrolling(self, player):
-        if (player.rect.right - self.offsetX >= WIDTH - SCROLL_AREA_WIDTH and player.x_vel > 0) or (player.rect.   left - self.offsetX <= SCROLL_AREA_WIDTH and player.x_vel < 0):
+        if (player.rect.right - self.offsetX >= WIDTH - SCROLL_AREA_WIDTH
+                and player.x_vel > 0) or (player.rect.left - self.offsetX <= SCROLL_AREA_WIDTH and player.x_vel < 0):
             self.offsetX += player.x_vel
 
 
--- src/services/GraphicsService/main.py
+++ src/services/GraphicsService/main.py
@@ -14,7 +14,7 @@ class GraphicsService:
         path = join('../assets', 'Terrain', 'Terrain.png')
         image = pygame.image.load(path).convert_alpha()
         surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
-        rect = pygame.Rect(96, 0, size, size)
+        rect = pygame.Rect(0, 64 + 64, size, size)
         surface.blit(image, (0, 0), rect)
 
         return pygame.transform.scale2x(surface)
